2ccb21398a5cfdc21fbb368817b60454736bb2a0
Charitha Bimsara <charithabimsara@gmail.com>
2025-03-21 02:59:59 +0530

    Updated commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ function Header() {
           {/* Logo */}
           <div className="flex items-center">
             <Link to="/" className="text-3xl font-bold text-white hover:text-purple-200 transition-all duration-300">
-              BG Remove
+              BG Remover
             </Link>
           </div>
 
